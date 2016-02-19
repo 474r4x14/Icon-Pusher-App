40237f661d671b60f407befa519460be03576d2a
null <null@v01d.uk>
2016-02-19 13:20:33 +0000

    Added https POST push

--- app/src/main/java/com/javatechig/listapps/AllAppsActivity.java
+++ app/src/main/java/com/javatechig/listapps/AllAppsActivity.java
@@ -1,29 +1,38 @@
 package com.javatechig.listapps;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.ComponentInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import android.provider.Settings.Secure;
+
 
 public class AllAppsActivity extends ListActivity {
 	private PackageManager packageManager = null;
@@ -69,13 +78,22 @@ public class AllAppsActivity extends ListActivity {
 	private void displayAboutDialog() {
 		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle(getString(R.string.about_title));
-		builder.setMessage(getString(R.string.about_desc));
+		builder.setMessage(getString(R.string.send_data));
 		
 		
 		builder.setPositiveButton("Know More", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
-				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://javatechig.com"));
-				startActivity(browserIntent);
+				//Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://javatechig.com"));
+				//startActivity(browserIntent);
+
+
+				//PushData pushData = new PushData();
+				//pushData.postData();
+				Log.w("myApp", "about to pushdata");
+				new PushData().execute("temp");
+				Log.w("myApp", "finished pushdata");
+
+
 				dialog.cancel();
 			}
 		});
@@ -87,6 +105,11 @@ public class AllAppsActivity extends ListActivity {
 		 
 		builder.show();
 	}
+
+
+
+
+
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -119,6 +142,11 @@ public class AllAppsActivity extends ListActivity {
 		//return builder.create();
 		builder.create().show();
 
+
+		String android_id = Secure.getString(this.getContentResolver(),
+				Secure.ANDROID_ID);
+
+		Log.w("android id?!",android_id);
 
 		/*
 		ApplicationInfo app = applist.get(position);
@@ -217,4 +245,117 @@ public class AllAppsActivity extends ListActivity {
 			super.onProgressUpdate(values);
 		}
 	}
+
+	private class PushData extends AsyncTask<String, Void, Void> {
+		@Override
+		protected Void doInBackground(String... urls) {
+
+			Log.w("myApp", "about to do http");
+
+
+
+
+//do this wherever you are wanting to POST
+			URL url;
+			HttpURLConnection conn;
+
+			try{
+//if you are using https, make sure to import java.net.HttpsURLConnection
+				url=new URL("https://defenestrate.me/icons.php");
+
+//you need to encode ONLY the values of the parameters
+				String param="param1=" + URLEncoder.encode("value1","UTF-8")+
+				"&param2="+URLEncoder.encode("value2","UTF-8")+
+				"&param3="+URLEncoder.encode("value3","UTF-8");
+
+				conn=(HttpURLConnection)url.openConnection();
+//set the output to true, indicating you are outputting(uploading) POST data
+				conn.setDoOutput(true);
+//once you set the output to true, you don’t really need to set the request method to post, but I’m doing it anyway
+				conn.setRequestMethod("POST");
+
+//Android documentation suggested that you set the length of the data you are sending to the server, BUT
+// do NOT specify this length in the header by using conn.setRequestProperty(“Content-Length”, length);
+//use this instead.
+				conn.setFixedLengthStreamingMode(param.getBytes().length);
+				conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//send the POST out
+
+				PrintWriter out = new PrintWriter(conn.getOutputStream());
+				out.print(param);
+				out.close();
+
+//build the string to store the response text from the server
+				String response= "";
+
+//start listening to the stream
+
+				Scanner inStream = new Scanner(conn.getInputStream());
+
+//process the stream and store it in StringBuilder
+				while(inStream.hasNextLine())
+					response+=(inStream.nextLine());
+
+
+				Log.w("myApp","response was " + response);
+			}
+//catch some error
+			catch(MalformedURLException ex){
+				//Toast.makeText(MyActivity.this, ex.toString(), 1 ).show();
+				ex.getStackTrace();
+
+			}
+// and some more
+			catch(IOException ex){
+ex.getStackTrace();
+				//Toast.makeText(MyActivity.this, ex.toString(), 1 ).show();
+			}
+
+
+
+
+
+
+			return null;
+
+		}
+
+
+		private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException
+		{
+			StringBuilder result = new StringBuilder();
+			boolean first = true;
+
+			for (NameValuePair pair : params)
+			{
+				if (first)
+					first = false;
+				else
+					result.append("&");
+
+				result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+				result.append("=");
+				result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+			}
+
+			return result.toString();
+		}
+
+
+
+
+
+
+		protected void onProgressUpdate(Integer... progress) {
+			//setProgressPercent(progress[0]);
+		}
+
+		protected void onPostExecute(Long result) {
+			//showDialog("Downloaded " + result + " bytes");
+		}
+	}
+
+
 }
+
+
