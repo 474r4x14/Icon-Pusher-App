fd2ad6509e535a652d56b33fa873144347a92ed8
null <null@v01d.uk>
2024-06-07 06:25:34 +0100

    Converted ApplicationAdapter to Kotlin

--- app/src/main/java/dev/southpaw/iconpusher/ApplicationAdapter.kt
+++ app/src/main/java/dev/southpaw/iconpusher/ApplicationAdapter.kt
@@ -1,68 +1,61 @@
-package dev.southpaw.iconpusher;
+package dev.southpaw.iconpusher
 
-import java.util.List;
+import android.content.Context
+import android.content.pm.PackageManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.ImageView;
-import android.widget.TextView;
+class ApplicationAdapter(
+    private val context: Context, textViewResourceId: Int,
+    val appsList: List<Request>
+) : ArrayAdapter<Request?>(
+    context, textViewResourceId, appsList!!
+) {
+//    private var appsList: List<Request>? = null
+    private val packageManager: PackageManager
 
-public class ApplicationAdapter extends ArrayAdapter<Request> {
-	private List<Request> appsList = null;
-	private final Context context;
-	private final PackageManager packageManager;
+    init {
+//        this.appsList = appsList
+        packageManager = context.packageManager
+    }
 
-	public ApplicationAdapter(Context context, int textViewResourceId,
-			List<Request> appsList) {
-		super(context, textViewResourceId, appsList);
-		this.context = context;
-		this.appsList = appsList;
-		packageManager = context.getPackageManager();
+    override fun getCount(): Int {
+        return (if ((null != appsList)) appsList.size else 0)
+    }
 
-	}
+    override fun getItem(position: Int): Request? {
+        return (if ((null != appsList)) appsList.get(position) else null)
+    }
 
-	@Override
-	public int getCount() {
-		return ((null != appsList) ? appsList.size() : 0);
-	}
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
 
-	@Override
-	public Request getItem(int position) {
-		return ((null != appsList) ? appsList.get(position) : null);
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View view = convertView;
-		if (null == view) {
-			LayoutInflater layoutInflater = (LayoutInflater) context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = layoutInflater.inflate(R.layout.snippet_list_row, null);
-		}
-		final Request request = appsList.get(position);
-		ApplicationInfo data = request.info;
-		if (null != data) {
-			TextView appName = (TextView) view.findViewById(R.id.app_name);
-			TextView packageName = (TextView) view.findViewById(R.id.app_paackage);
-			ImageView iconview = (ImageView) view.findViewById(R.id.app_icon);
-			CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkbox);
-			AppListerActivity.checkboxes.add(checkBox);
-			appName.setText(data.loadLabel(packageManager));
-			packageName.setText(data.packageName);
-			iconview.setImageDrawable(data.loadIcon(packageManager));
-			checkBox.setChecked(request.selected);
-		}
-		return view;
-	}
-};
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        if (null == view) {
+            val layoutInflater = context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            view = layoutInflater.inflate(R.layout.snippet_list_row, null)
+        }
+        val request = appsList!![position]
+        val data = request.info
+        if (null != data) {
+            val appName = view!!.findViewById<View>(R.id.app_name) as TextView
+            val packageName = view.findViewById<View>(R.id.app_paackage) as TextView
+            val iconview = view.findViewById<View>(R.id.app_icon) as ImageView
+            val checkBox = view.findViewById<View>(R.id.checkbox) as CheckBox
+            AppListerActivity.checkboxes.add(checkBox)
+            appName.text = data.loadLabel(packageManager)
+            packageName.text = data.packageName
+            iconview.setImageDrawable(data.loadIcon(packageManager))
+            checkBox.isChecked = request.selected
+        }
+        return view!!
+    }
+}
